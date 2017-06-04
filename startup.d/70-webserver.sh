27d5c579cd56332346f68603ae8ed23e92d47775
md678685 <mathew678@hotmail.co.uk>
2017-06-04 18:31:09 +0100

    Use symbolic log links
    Weren't supposed to be hard links in the first place

--- startup.d/70-webserver.sh
+++ startup.d/70-webserver.sh
@@ -2,8 +2,8 @@
 
 pkill chaos_server
 
-ln -f /var/log/nginx/access.log /root/workspace/Chaos/log/nginx-access.log
-ln -f /var/log/nginx/error.log /root/workspace/Chaos/log/nginx-error.log
+ln -sf /var/log/nginx/access.log /root/workspace/Chaos/log/nginx-access.log
+ln -sf /var/log/nginx/error.log /root/workspace/Chaos/log/nginx-error.log
 
 nginx -t
 service nginx force-reload
