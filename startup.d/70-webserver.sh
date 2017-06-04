4da362600fafa00ecccffa0087cf1a547f8503c0
md678685 <mathew678@hotmail.co.uk>
2017-06-04 18:06:16 +0100

    Link nginx logs to log dir

--- startup.d/70-webserver.sh
+++ startup.d/70-webserver.sh
@@ -2,7 +2,8 @@
 
 pkill chaos_server
 
-chmod -R a+rX server/
+ln -f /var/log/nginx/access.log /root/workspace/Chaos/log/nginx-access.log
+ln -f /var/log/nginx/error.log /root/workspace/Chaos/log/nginx-error.log
 
 nginx -t
 service nginx force-reload
